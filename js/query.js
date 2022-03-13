b27508a8e641d8a6f0b2f7c4b8852e3d4f26de23
charles okunzo <okunzopeter01@gmail.com>
2022-03-13 11:13:49 +0300

    style:overlay-add overlay on portfolio cards

--- js/query.js
+++ js/query.js
@@ -22,11 +22,26 @@ $(document).ready(()=>{
 
   $('#prod-mgt-img').click(()=>{
     $('#prod-mgt-img').toggle();
-    $('#management').toggle();
+    $('#p-management').toggle();
   })
 
-  $('#management').click(()=>{
-    $('#management').toggle();
+  $('#p-management').click(()=>{
+    $('#p-management').toggle();
     $('#prod-mgt-img').toggle();
+  })
+
+  $('#b-expanse').hover(()=>{
+    $('#b-expanse-txt').fadeIn(1000);
+    $('#overlay1').slideDown(1000);
+  }, ()=>{
+    $('#b-expanse-txt').fadeOut(1000);
+    $('#overlay1').slideUp(1000);
+  })
+
+  $('#m-ontario').hover(()=>{
+    $('#m-ontario-txt').fadeIn(1000);
+    $('#m-ontario-txt').addClass('ontario-wrap');
+  }, ()=>{
+    $('#m-ontario-txt').fadeOut(1000)
   })
 })
