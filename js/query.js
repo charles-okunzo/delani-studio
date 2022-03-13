c51d9023593e74f46c400b5ede0ba7d4a71ae62d
charles okunzo <okunzopeter01@gmail.com>
2022-03-13 09:57:42 +0300

    feature:development-enable toggle between development image and description

--- js/query.js
+++ js/query.js
@@ -4,8 +4,21 @@ $(document).ready(()=>{
     $('#design-img').toggle();
     $('#design').toggle();
   })
+
   $('#design').click(()=>{
     $('#design').toggle();
     $('#design-img').toggle();
   })
+
+  $('#dev-img').click(()=>{
+    $('#dev-img').toggle();
+    $('#development').toggle();
+  })
+
+  $('#development').click(()=>{
+    $('#development').toggle();
+    $('#dev-img').toggle();
+  })
+
+  
 })
