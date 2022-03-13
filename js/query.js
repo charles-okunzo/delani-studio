74753d70551a1725b507edeca05b5d66440b4a88
charles okunzo <okunzopeter01@gmail.com>
2022-03-13 10:01:48 +0300

    feature:product mgt-enable toggle between product management image and description

--- js/query.js
+++ js/query.js
@@ -20,5 +20,13 @@ $(document).ready(()=>{
     $('#dev-img').toggle();
   })
 
-  
+  $('#prod-mgt-img').click(()=>{
+    $('#prod-mgt-img').toggle();
+    $('#management').toggle();
+  })
+
+  $('#management').click(()=>{
+    $('#management').toggle();
+    $('#prod-mgt-img').toggle();
+  })
 })
